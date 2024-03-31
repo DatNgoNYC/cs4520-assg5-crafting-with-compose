801a800b7a9dcb20995b72a8714747d56e362989
Dat Ngo <datngonyc@gmail.com>
2024-03-31 19:04:00 -0400

    Create worker to add products to database

--- app/src/main/java/com/cs4520/assignment5/Work/ProductFetchWorker.kt
+++ app/src/main/java/com/cs4520/assignment5/Work/ProductFetchWorker.kt
@@ -1,4 +1,24 @@
 package com.cs4520.assignment5.Work
 
-class FetchProductsWorker () {
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.cs4520.assignment4.Data.ProductRepository
+
+class FetchProductsWorker (appContext: Context, workerParameters: WorkerParameters):
+    CoroutineWorker(appContext, workerParameters) {
+    override suspend fun doWork(): Result {
+        return try {
+            val repository: ProductRepository = ProductRepository(applicationContext)
+            repository.addRandomProductListToCurrentAndReturnAllProducts()
+
+            Result.success()
+        } catch (e: Exception) {
+            Log.d("WorkerManager", "${e.message}")
+            Result.failure()
+        }
+
+    }
+
 }
