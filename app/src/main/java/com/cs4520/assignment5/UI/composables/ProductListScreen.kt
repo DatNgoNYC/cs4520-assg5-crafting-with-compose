c57133fed4108338b61a658ea08e8dd2d2d6dd9f
Dat Ngo <datngonyc@gmail.com>
2024-03-31 16:16:21 -0400

    Add product list uiState.

--- app/src/main/java/com/cs4520/assignment5/UI/composables/ProductListScreen.kt
+++ app/src/main/java/com/cs4520/assignment5/UI/composables/ProductListScreen.kt
@@ -4,7 +4,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cs4520.assignment5.ViewModels.ProductListViewModel
 
 @Composable
-fun ProductListScreen() {
+fun ProductListScreen(productListViewModel: ProductListViewModel = viewModel()) {
+    val uiState by productListViewModel.products.collectAs
+
+    // collect the state variables from the view model to listen for changes to compose
+
+    // use LaunchedEffects to load the products
+
+    // ui stuff using the state variables
+
 }
+
+@Composable
+fun ProductList() {
+
+}
+
+@Composable
+fun ProductItem() {
+
+}
+
--- app/src/main/java/com/cs4520/assignment5/ViewModels/ProductListViewModel.kt
+++ app/src/main/java/com/cs4520/assignment5/ViewModels/ProductListViewModel.kt
@@ -1,7 +1,42 @@
 package com.cs4520.assignment5.ViewModels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cs4520.assignment4.Data.Entities.Product
+import com.cs4520.assignment4.Data.ProductRepository
+import kotlinx.coroutines.launch
 
-class ProductListViewModel : ViewModel() {
+class ProductListViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository : ProductRepository = ProductRepository(application.applicationContext)
+    private val _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>> = _products
+    private val _isLoading = MutableLiveData<Boolean>(true)
+    val isLoading: LiveData<Boolean> = _isLoading
 
+    fun loadProducts() {
+
+        viewModelScope.launch {
+            try {
+                val results = repository.getAllProducts()
+                if (results.isEmpty()) {
+                    _isLoading.postValue(false)
+                } else {
+                    _isLoading.postValue(false)
+                    _products.postValue(results)
+                }
+            } catch (e: Error) {
+                _isLoading.postValue(false)
+            }
+        }
+    }
 }
+
+data class ProductListUiState(
+    val isLoading: Boolean = false,
+    val products: List<Product> = emptyList(),
+    val error: String? = null
+)
